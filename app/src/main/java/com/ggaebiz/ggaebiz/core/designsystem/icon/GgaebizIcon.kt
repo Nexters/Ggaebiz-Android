0a6311b4c42358a8557635feed989e363fcdfb34
seoggyui <likppi10@gmail.com>
2025-01-29 23:51:46 +0900

    [chore] 백버튼 아이콘 추가

---
+++ app/src/main/java/com/ggaebiz/ggaebiz/core/designsystem/icon/GgaebizIcon.kt
@@ -0,0 +1,10 @@
+package com.ggaebiz.ggaebiz.core.designsystem.icon
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import com.ggaebiz.ggaebiz.R
+
+object GgaebizIcon {
+    @Composable
+    fun icBack() = painterResource(R.drawable.ic_back)
+}
