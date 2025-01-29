599eaa093636b9cf1f0620a74b1436fabf5106f4
seoggyui <likppi10@gmail.com>
2025-01-29 23:54:50 +0900

    [chore] 메인 액티비티 수정 (테스트 목적)

--- app/src/main/java/com/ggaebiz/ggaebiz/presentation/MainActivity.kt
+++ app/src/main/java/com/ggaebiz/ggaebiz/presentation/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.ggaebiz.ggaebiz.presentation.ui.theme.GgaebizTheme
+import androidx.compose.ui.unit.dp
+import com.ggaebiz.ggaebiz.R
+import com.ggaebiz.ggaebiz.core.designsystem.component.GgaebizIconButton
+import com.ggaebiz.ggaebiz.core.designsystem.component.GgaebizTopAppBar
+import com.ggaebiz.ggaebiz.core.designsystem.icon.GgaebizIcon
+import com.ggaebiz.ggaebiz.core.designsystem.theme.GgaebizTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,15 +39,35 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting(
     name: String,
     modifier: Modifier = Modifier,
 ) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
+    Column {
+        GgaebizTopAppBar(
+            titleDrawable = R.drawable.ggaebiz_kor,
+//            titleRes = R.string.setting_title,
+            modifier = Modifier,
+            navigationIcon = {
+                GgaebizIconButton(
+                    modifier = Modifier.size(48.dp),
+                    onClick = {},
+                    icon = {
+                        Icon(
+                            painter = GgaebizIcon.icBack(),
+                            contentDescription = null,
+                        )
+                    },
+                )
+            },
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+        )
+    }
 }
 
 @Preview(showBackground = true)
