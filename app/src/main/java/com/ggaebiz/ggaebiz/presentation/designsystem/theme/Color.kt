91b0398c3c99431103b930be98490bbd5a3d222a
asheon12 <55980680+ashwon12@users.noreply.github.com>
2025-02-01 01:29:01 +0900

    [feat] 커스텀 컬러 지정

--- app/src/main/java/com/ggaebiz/ggaebiz/presentation/designsystem/theme/Color.kt
+++ app/src/main/java/com/ggaebiz/ggaebiz/presentation/designsystem/theme/Color.kt
@@ -1,99 +1,109 @@
 package com.ggaebiz.ggaebiz.presentation.designsystem.theme
 
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val White = Color(0xFFFFFFFF)
-val White_8 = Color(0x14FFFFFF)
-val White_12 = Color(0x1EFFFFFF)
-val White_16 = Color(0x28FFFFFF)
+@Immutable
+data class GaeBizColors(
+    val primaryOrange : Color,
 
-val Black = Color(0xFF000000)
-val Black_8 = Color(0x14000000)
-val Black_12 = Color(0x1E000000)
-val Black_16 = Color(0x28000000)
-val Black_20 = Color(0x32000000)
-val Black_24 = Color(0x3C000000)
-val Black_28 = Color(0x46000000)
-val Black_32 = Color(0x50000000)
-val Black_36 = Color(0x5A000000)
-val Black_40 = Color(0x64000000)
-val Black_44 = Color(0x6E000000)
+    val white: Color,
+    val white8: Color,
+    val white12: Color,
+    val white16: Color,
 
-val Orange50 = Color(0xFFFFF1EC)
-val Orange100 = Color(0xFFFED2C3)
-val Orange200 = Color(0xFFFEBDA6)
-val Orange300 = Color(0xFFFD9F7D)
-val Orange400 = Color(0xFFFD8C64)
-val Orange500 = Color(0xFFFC6F3D)
-val Orange600 = Color(0xFFE56538)
-val Orange700 = Color(0xFFB34F2B)
-val Orange800 = Color(0xFF8B3D22)
-val Orange900 = Color(0xFF6A2F1A)
+    val black: Color,
+    val black8: Color,
+    val black12: Color,
+    val black16: Color,
+    val black20: Color,
+    val black24: Color,
+    val black28: Color,
+    val black32: Color,
+    val black36: Color,
+    val black40: Color,
+    val black44: Color,
 
-val Gray25 = Color(0xFFF7F7F7)
-val Gray50 = Color(0xFFF1F1F2)
-val Gray75 = Color(0xFFE3E4E5)
-val Gray100 = Color(0xFFD3D4D6)
-val Gray200 = Color(0xFFBDBFC3)
-val Gray300 = Color(0xFFA0A2A8)
-val Gray400 = Color(0xFF888B94)
-val Gray500 = Color(0xFF70737C)
-val Gray600 = Color(0xFF666971)
-val Gray700 = Color(0xFF505258)
-val Gray800 = Color(0xFF3E3F44)
-val Gray900 = Color(0xFF2F3034)
+    val gray25: Color,
+    val gray50: Color,
+    val gray75: Color,
+    val gray100: Color,
+    val gray200: Color,
+    val gray300: Color,
+    val gray400: Color,
+    val gray500: Color,
+    val gray600: Color,
+    val gray700: Color,
+    val gray800: Color,
+    val gray900: Color,
+)
 
-val Blue50 = Color(0xFFE7F0FF)
-val Blue100 = Color(0xFFB4D0FF)
-val Blue200 = Color(0xFF90B9FF)
-val Blue300 = Color(0xFF619AFF)
-val Blue400 = Color(0xFF3D85FF)
-val Blue500 = Color(0xFF0D66FF)
-val Blue600 = Color(0xFF0C5BE3)
-val Blue700 = Color(0xFF0948B5)
-val Blue800 = Color(0xFF07388C)
-val Blue900 = Color(0xFF052B6B)
+val LocalGaeBizColor = staticCompositionLocalOf {
+    GaeBizColors(
+        primaryOrange = Color.Unspecified,
 
-val Red50 = Color(0xFFFFE9E8)
-val Red100 = Color(0xFFFDBAB8)
-val Red200 = Color(0xFFFD9895)
-val Red300 = Color(0xFFFC7672)
-val Red400 = Color(0xFFF7554F)
-val Red500 = Color(0xFFFA2019)
-val Red600 = Color(0xFFE41D17)
-val Red700 = Color(0xFFB21712)
-val Red800 = Color(0xFF8A120E)
-val Red900 = Color(0xFF690D0B)
+        white = Color.Unspecified,
+        white8 = Color.Unspecified,
+        white12 = Color.Unspecified,
+        white16 = Color.Unspecified,
 
-val Green50 = Color(0xFFE8FCEB)
-val Green100 = Color(0xFFB7F5C1)
-val Green200 = Color(0xFF94F1A3)
-val Green300 = Color(0xFF74E888)
-val Green400 = Color(0xFF4EE567)
-val Green500 = Color(0xFF16DE37)
-val Green600 = Color(0xFF14CC33)
-val Green700 = Color(0xFF109F28)
-val Green800 = Color(0xFF0C7B1F)
-val Green900 = Color(0xFF095E18)
+        black = Color.Unspecified,
+        black8 = Color.Unspecified,
+        black12 = Color.Unspecified,
+        black16 = Color.Unspecified,
+        black20 = Color.Unspecified,
+        black24 = Color.Unspecified,
+        black28 = Color.Unspecified,
+        black32 = Color.Unspecified,
+        black36 = Color.Unspecified,
+        black40 = Color.Unspecified,
+        black44 = Color.Unspecified,
 
-val Purple50 = Color(0xFFF6EDFF)
-val Purple100 = Color(0xFFE3C8FF)
-val Purple200 = Color(0xFFD6ADFF)
-val Purple300 = Color(0xFFC58CFF)
-val Purple400 = Color(0xFFB870FF)
-val Purple500 = Color(0xFFA64DFF)
-val Purple600 = Color(0xFF9746E8)
-val Purple700 = Color(0xFF7637B5)
-val Purple800 = Color(0xFF5B2A8C)
-val Purple900 = Color(0xFF46206B)
+        gray25 = Color.Unspecified,
+        gray50 = Color.Unspecified,
+        gray75 = Color.Unspecified,
+        gray100 = Color.Unspecified,
+        gray200 = Color.Unspecified,
+        gray300 = Color.Unspecified,
+        gray400 = Color.Unspecified,
+        gray500 = Color.Unspecified,
+        gray600 = Color.Unspecified,
+        gray700 = Color.Unspecified,
+        gray800 = Color.Unspecified,
+        gray900 = Color.Unspecified
+    )
+}
 
-val Pink50 = Color(0xFFFFEDFF)
-val Pink100 = Color(0xFFFDC7FD)
-val Pink200 = Color(0xFFFDACFD)
-val Pink300 = Color(0xFFFC88FC)
-val Pink400 = Color(0xFFFB71FB)
-val Pink500 = Color(0xFFFA4BFA)
-val Pink600 = Color(0xFFE444E4)
-val Pink700 = Color(0xFFB235B2)
-val Pink800 = Color(0xFF8A298A)
-val Pink900 = Color(0xFF692069)
+val GaeBizColorScheme = GaeBizColors(
+    primaryOrange = Color(0xFFFC6F3D),
+    white = Color(0xFFFFFFFF),
+    white8 = Color(0x14FFFFFF),
+    white12 = Color(0x1EFFFFFF),
+    white16 = Color(0x28FFFFFF),
+
+    black = Color(0xFF000000),
+    black8 = Color(0x14000000),
+    black12 = Color(0x1E000000),
+    black16 = Color(0x28000000),
+    black20 = Color(0x32000000),
+    black24 = Color(0x3C000000),
+    black28 = Color(0x46000000),
+    black32 = Color(0x50000000),
+    black36 = Color(0x5A000000),
+    black40 = Color(0x64000000),
+    black44 = Color(0x6E000000),
+
+    gray25 = Color(0xFFF7F7F7),
+    gray50 = Color(0xFFF1F1F2),
+    gray75 = Color(0xFFE3E4E5),
+    gray100 = Color(0xFFD3D4D6),
+    gray200 = Color(0xFFBDBFC3),
+    gray300 = Color(0xFFA0A2A8),
+    gray400 = Color(0xFF888B94),
+    gray500 = Color(0xFF70737C),
+    gray600 = Color(0xFF666971),
+    gray700 = Color(0xFF505258),
+    gray800 = Color(0xFF3E3F44),
+    gray900 = Color(0xFF2F3034)
+)
