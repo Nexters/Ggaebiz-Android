7e08bc232a3d695b20a458e1c541b4b17e832f6d
seoggyui <likppi10@gmail.com>
2025-02-02 16:42:35 +0900

    [feat] PointText.kt, 캐릭터 특징 텍스트 추가

---
+++ app/src/main/java/com/ggaebiz/ggaebiz/presentation/ui/PointText.kt
@@ -0,0 +1,55 @@
+package com.ggaebiz.ggaebiz.presentation.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ggaebiz.ggaebiz.presentation.designsystem.theme.GaeBizTheme
+
+@Composable
+fun GaeBizPointText(
+    modifier: Modifier = Modifier,
+    text: String,
+    textStyle: TextStyle,
+    radius: Int = 10,
+    textColor: Color = GaeBizTheme.colors.gray900,
+    backgroundColor: Color = GaeBizTheme.colors.gray75,
+) {
+    Box(
+        modifier = modifier
+            .wrapContentSize()
+            .background(
+                color = backgroundColor,
+                shape = RoundedCornerShape(radius.dp),
+            )
+            .padding(
+                horizontal = 10.dp,
+                vertical = 8.dp,
+            ),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = text,
+            color = textColor,
+            style = textStyle,
+        )
+    }
+}
+
+@Preview("Point Text")
+@Composable
+private fun GaeBizPointTextPreview() {
+    GaeBizPointText(
+        text = "까불이",
+        textStyle = GaeBizTheme.typography.bodySemiBold,
+    )
+}
