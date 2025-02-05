da2ce739a5ca1adae89f33237160e847a85ddbbd
asheon12 <55980680+ashwon12@users.noreply.github.com>
2025-02-05 15:52:05 +0900

    [feat] koin 적용

---
+++ app/src/main/java/com/ggaebiz/ggaebiz/GgaeBizApp.kt
@@ -0,0 +1,27 @@
+package com.ggaebiz.ggaebiz
+
+import android.app.Application
+import com.ggaebiz.ggaebiz.data.datastore.AudioDataStore
+import com.ggaebiz.ggaebiz.data.datastore.DataStoreObject
+import com.ggaebiz.ggaebiz.di.appModule
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.getKoin
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class GgaeBizApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@GgaeBizApp)
+            modules(appModule)
+        }
+
+        GlobalScope.launch {
+            val audioDataStore: AudioDataStore = getKoin().get()
+            DataStoreObject.initialize(this@GgaeBizApp, audioDataStore)
+        }
+    }
+}
---
+++ app/src/main/java/com/ggaebiz/ggaebiz/di/AppModule.kt
@@ -0,0 +1,21 @@
+package com.ggaebiz.ggaebiz.di
+
+import android.content.Context
+import androidx.datastore.preferences.preferencesDataStore
+import com.ggaebiz.ggaebiz.data.datastore.AudioDataStore
+import com.ggaebiz.ggaebiz.data.datastore.DataStoreObject.audioDataStore
+import com.ggaebiz.ggaebiz.data.repository.AudioRepositoryImpl
+import com.ggaebiz.ggaebiz.domain.repository.AudioRepository
+import com.ggaebiz.ggaebiz.domain.usecase.GetAudioResIdUseCase
+import com.ggaebiz.ggaebiz.presentation.TimerViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.module
+
+val appModule = module {
+    single { androidContext().audioDataStore }
+    single { AudioDataStore(get()) }
+    single<AudioRepository> { AudioRepositoryImpl(androidContext(), get()) }
+    single { GetAudioResIdUseCase(get()) }
+    viewModel { TimerViewModel(get()) }
+}
