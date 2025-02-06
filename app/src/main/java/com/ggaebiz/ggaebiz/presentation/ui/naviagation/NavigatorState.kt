920ea802b6cc2065817ecdbcbed34d6fe98ea254
asheon12 <55980680+ashwon12@users.noreply.github.com>
2025-02-07 02:35:50 +0900

    [feat] navHost 설정

---
+++ app/src/main/java/com/ggaebiz/ggaebiz/presentation/ui/naviagation/NavigatorState.kt
@@ -0,0 +1,47 @@
+package com.ggaebiz.ggaebiz.presentation.ui.naviagation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun rememberNavigator(
+    navController: NavHostController = rememberNavController(),
+): NavigatorState = remember(navController) {
+    NavigatorState(
+        navController = navController,
+    )
+}
+
+class NavigatorState(
+    val navController: NavHostController,
+) {
+    private val currentDestination: NavDestination?
+        @Composable get() = navController
+            .currentBackStackEntryAsState().value?.destination
+
+    val startDestination = Route.Splash
+
+    fun navigateHome() {
+        navController.navigate(Route.Home)
+    }
+
+    fun navigateSetting() {
+        navController.navigate(Route.Setting)
+    }
+
+    fun navigateTimer() {
+        navController.navigate(Route.Timer)
+    }
+
+    fun navigateAlarm() {
+        navController.navigate(Route.Alarm)
+    }
+
+    fun popBackStack() {
+        navController.popBackStack()
+    }
+}
