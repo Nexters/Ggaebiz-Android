ca20849fcd74c3f8203b58ffee44de0e4bede42d
seoggyui <likppi10@gmail.com>
2025-02-10 02:16:49 +0900

    [fix] 어아콘 다시 추가

---
+++ app/src/main/java/com/ggaebiz/ggaebiz/presentation/designsystem/component/icon/GaeBizIcon.kt
@@ -0,0 +1,20 @@
+package com.ggaebiz.ggaebiz.presentation.designsystem.component.icon
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.ggaebiz.ggaebiz.R
+
+object GaeBizIcon {
+
+    val icBack: ImageVector
+        @Composable
+        get() = ImageVector.vectorResource(R.drawable.ic_back)
+    val icRightArrow: ImageVector
+        @Composable
+        get() = ImageVector.vectorResource(R.drawable.ic_right_arrow)
+
+    val icPolygon: ImageVector
+        @Composable
+        get() = ImageVector.vectorResource(R.drawable.ic_polygon)
+}
