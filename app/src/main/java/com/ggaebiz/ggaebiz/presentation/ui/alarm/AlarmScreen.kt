b80c6030da9a389449f71ae5191ca9a9083db0d2
asheon12 <55980680+ashwon12@users.noreply.github.com>
2025-02-10 22:17:21 +0900

    [feat] AlarmScreen + AlarmViewModel

--- app/src/main/java/com/ggaebiz/ggaebiz/presentation/ui/alarm/AlarmScreen.kt
+++ app/src/main/java/com/ggaebiz/ggaebiz/presentation/ui/alarm/AlarmScreen.kt
@@ -1,29 +1,32 @@
 package com.ggaebiz.ggaebiz.presentation.ui.alarm
 
 import android.net.Uri
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
-import com.ggaebiz.ggaebiz.data.model.Character
+import com.ggaebiz.ggaebiz.R
 import com.ggaebiz.ggaebiz.presentation.common.extension.collectAsStateWithLifecycle
 import com.ggaebiz.ggaebiz.presentation.common.extension.collectSideEffectWithLifecycle
 import com.ggaebiz.ggaebiz.presentation.common.extension.getRawResId
+import com.ggaebiz.ggaebiz.presentation.designsystem.theme.GaeBizTheme
+import com.ggaebiz.ggaebiz.presentation.designsystem.ui.FullScreen
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
-fun AlarmScreen(viewModel: AlarmViewModel = koinViewModel()) {
+fun AlarmScreen(
+    navigateStart: () -> Unit,
+    viewModel: AlarmViewModel = koinViewModel(),
+) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     val context = LocalContext.current
 
     viewModel.sideEffects.collectSideEffectWithLifecycle { effect ->
         when (effect) {
             is AlarmSideEffect.PlayAudio -> {
+                // TODO :: 분리 필요
                 ExoPlayer.Builder(context).build().apply {
                     val resInt = context.getRawResId(effect.resId)
                     val mediaItem = MediaItem.fromUri(
@@ -32,15 +35,49 @@ fun AlarmScreen(viewModel: AlarmViewModel = koinViewModel()) {
                     setMediaItem(mediaItem)
                     prepare()
                     play()
+
                 }
+            }
+            AlarmSideEffect.ClickFinish -> {
+                navigateStart()
+            }
+            AlarmSideEffect.ClickSnooze -> {
+                navigateStart()
             }
         }
     }
+    AlarmContent(
+        uiState = uiState,
+        onClickFinishButton = { viewModel.finishTimer() },
+        onClickSnoozeButton = { viewModel.snoozeTimer() }
+    )
+}
 
-    Button(
-        modifier = Modifier.systemBarsPadding(),
-        onClick = { viewModel.startTimer(Character.KIKI, 3) }
+
+@Composable
+fun AlarmContent(
+    uiState: AlarmState,
+    onClickFinishButton: () -> Unit,
+    onClickSnoozeButton: () -> Unit,
+) {
+    FullScreen(
+        backGroundImage = uiState.backGroundImgRes
     ) {
-        Text(uiState.text)
+        AlarmTopSection(
+            ment = uiState.ment,
+            plusSecond = uiState.plusSeconds
+        )
+        AlarmBottomSection(
+            onClickFinishButton = onClickFinishButton,
+            onClickSnoozeButton = onClickSnoozeButton,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AlarmScreenPreview() {
+    GaeBizTheme {
+        AlarmScreen({})
     }
 }
--- app/src/main/java/com/ggaebiz/ggaebiz/presentation/ui/alarm/AlarmViewModel.kt
+++ app/src/main/java/com/ggaebiz/ggaebiz/presentation/ui/alarm/AlarmViewModel.kt
@@ -1,27 +1,78 @@
 package com.ggaebiz.ggaebiz.presentation.ui.alarm
 
+import com.ggaebiz.ggaebiz.R
 import com.ggaebiz.ggaebiz.data.model.Character
 import com.ggaebiz.ggaebiz.domain.usecase.GetAudioResIdUseCase
 import com.ggaebiz.ggaebiz.presentation.common.base.BaseViewModel
+import kotlinx.coroutines.delay
+import java.util.Locale
 
 data class AlarmState(
-    val text: String = "타이머를 울려주세요",
+    // TODO :: ment, image 연동
+    val ment : String = "A ~~ yo ! ! !  계획만 천재야, 행동은 문제야, 일어나지 않으면 오늘도 넌 패배야",
+    val plusSeconds : String = "+ 00:00",
+    val backGroundImgRes : Int = R.drawable.fullpage_kiki_lev_1
 )
 
 sealed interface AlarmSideEffect {
     data class PlayAudio(val resId: String) : AlarmSideEffect
+    data object ClickSnooze : AlarmSideEffect
+    data object ClickFinish : AlarmSideEffect
 }
 
 class AlarmViewModel(
     private val getAudioResIdUseCase: GetAudioResIdUseCase,
 ) : BaseViewModel<AlarmState,Nothing,AlarmSideEffect>(AlarmState()) {
 
+    init {
+        startIncreaseSeconds()
+    }
+
     fun startTimer(
         character: Character,
         level: Int,
     ) = launch {
         val resIds = getAudioResIdUseCase(character, level, 0)
-        updateState{ it.copy(text = "타이머 울리는 중") }
+        updateState{ it.copy(ment = "타이머 울리는 중") }
         postSideEffect(AlarmSideEffect.PlayAudio(resIds))
     }
+
+    private fun startIncreaseSeconds() = launch {
+        while (true) {
+            delay(1000L) // 1초 대기
+            updateState {
+                val (minute, second) = parseTime(it.plusSeconds)
+                val newSecond = second + 1
+                val newPlusSeconds = formatTime(newSecond,minute)
+                it.copy(plusSeconds = newPlusSeconds)
+            }
+        }
+    }
+
+    fun finishTimer(){
+        postSideEffect(AlarmSideEffect.ClickFinish)
+    }
+
+    fun snoozeTimer(){
+        postSideEffect(AlarmSideEffect.ClickSnooze)
+    }
+
+    private fun formatTime(seconds: Int, minute : Int): String {
+        val newMinute = if (seconds >= 60) minute + 1 else minute
+        val newSecondFormatted = seconds % 60
+        return String.format(Locale.KOREA,"+ %02d:%02d", newMinute, newSecondFormatted)
+    }
+
+    private fun parseTime(time: String): Pair<Int, Int> {
+        val regex = "\\+ (\\d{2}):(\\d{2})".toRegex()
+        val matchResult = regex.find(time)
+        return if (matchResult != null) {
+            val minute = matchResult.groupValues[1].toInt()
+            val second = matchResult.groupValues[2].toInt()
+            Pair(minute, second)
+        } else {
+            Pair(0, 0) // 기본값
+        }
+    }
+
 }
