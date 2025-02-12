c11a69844006970b4374ad5c91dd88b65400ae79
asheon12 <55980680+ashwon12@users.noreply.github.com>
2025-02-13 03:31:03 +0900

    [feat] 알림 권한 얻기

--- app/src/main/java/com/ggaebiz/ggaebiz/presentation/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/ggaebiz/ggaebiz/presentation/ui/home/HomeViewModel.kt
@@ -1,29 +1,38 @@
 package com.ggaebiz.ggaebiz.presentation.ui.home
 
+import com.ggaebiz.ggaebiz.domain.usecase.GetTimerStateUseCase
 import com.ggaebiz.ggaebiz.domain.usecase.SelectCharacterIdxUseCase
 import com.ggaebiz.ggaebiz.presentation.common.base.BaseViewModel
 
 data class HomeState(
     val selectCharacterIdx: Int = 0,
+    val isGranted: Boolean = false,
 )
 
 sealed interface HomeSideEffect {
     data object NavigateToSetting : HomeSideEffect
+    data object CheckPermission : HomeSideEffect
 }
 
 sealed interface HomeIntent {
     data class SelectCharacter(val selectCharacterIdx: Int) : HomeIntent
     data object ClickSettingButton : HomeIntent
+    data class UpdatePermission(val isGranted: Boolean) : HomeIntent
 }
 
 class HomeViewModel(
     private val selectCharacterIdxUseCase: SelectCharacterIdxUseCase,
 ) : BaseViewModel<HomeState, HomeIntent, HomeSideEffect>(HomeState()) {
 
+    init {
+        postSideEffect(HomeSideEffect.CheckPermission)
+    }
+
     fun processIntent(intent: HomeIntent) {
         when (intent) {
             is HomeIntent.ClickSettingButton -> clickSettingButton()
             is HomeIntent.SelectCharacter -> selectCharacter(intent.selectCharacterIdx)
+            is HomeIntent.UpdatePermission -> updateState { it.copy(isGranted = intent.isGranted) }
         }
     }
 
@@ -35,4 +44,5 @@ class HomeViewModel(
         selectCharacterIdxUseCase(uiState.value.selectCharacterIdx)
         postSideEffect(HomeSideEffect.NavigateToSetting)
     }
+
 }
