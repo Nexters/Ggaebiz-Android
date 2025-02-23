ef8e4d1dae490b8f5952a528a08b2da1ed81bdf9
seoggyui <likppi10@gmail.com>
2025-02-23 19:34:27 +0900

    [hotfix] 잠금화면 권한 설정

--- app/src/main/java/com/ggaebiz/ggaebiz/presentation/service/TimerService.kt
+++ app/src/main/java/com/ggaebiz/ggaebiz/presentation/service/TimerService.kt
@@ -5,15 +5,16 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.content.pm.ServiceInfo
 import android.net.Uri
 import android.os.Binder
 import android.os.Build
 import android.os.IBinder
+import android.os.PowerManager
 import android.util.Log
 import androidx.core.app.NotificationCompat
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
@@ -41,6 +42,7 @@ class TimerService : Service() {
 
     private lateinit var _notificationManager : NotificationManager
     val notificationManager: NotificationManager get() = _notificationManager
+    private lateinit var wakeLock: PowerManager.WakeLock
 
     companion object {
         const val ACTION_START = "START_TIMER"
@@ -66,6 +68,10 @@ class TimerService : Service() {
     override fun onCreate() {
         super.onCreate()
         Log.d("TimerService", "onCreate()")
+
+        val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
+        wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "TimerService::WakelockTag")
+        wakeLock.acquire()
 
         // Notification 채널 생성
         _notificationManager = getSystemService(NotificationManager::class.java)
@@ -187,7 +193,7 @@ class TimerService : Service() {
             val channel = NotificationChannel(
                 NOTIFICATION_CHANNEL_ID,
                 "Timer Service",
-                NotificationManager.IMPORTANCE_DEFAULT
+                NotificationManager.IMPORTANCE_HIGH
             )
             notificationManager.createNotificationChannel(channel)
         }
@@ -197,10 +203,11 @@ class TimerService : Service() {
         return NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
             .setContentTitle(getString(R.string.notification_title))
             .setContentText(getString(R.string.notification_default_content))
-            .setPriority(NotificationCompat.PRIORITY_LOW) // 우선순위 최소
-            .setVisibility(NotificationCompat.VISIBILITY_SECRET) // 잠금 화면에서도 숨김
+            .setPriority(NotificationCompat.PRIORITY_HIGH) // 우선순위 최소
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC) // 잠금 화면에서도 숨김
             .setSilent(true) // 알림 사운드 없음
             .setSmallIcon(R.mipmap.ic_app_icon_round)
+            .setOngoing(true)
             .build()
     }
 
@@ -209,6 +216,7 @@ class TimerService : Service() {
         timerJob?.cancel()
         overSecondsJob?.cancel()
         player.release()
+        wakeLock.release()
         super.onDestroy()
     }
 }
