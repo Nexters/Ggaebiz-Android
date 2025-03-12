b1416253f6716c26d7adbb72ceb15bb31acbe1b6
seoggyui <likppi10@gmail.com>
2025-03-12 23:27:51 +0900

    [fix] homeViewModel intent 및 sideEffect 수정

--- app/src/main/java/com/ggaebiz/ggaebiz/presentation/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/ggaebiz/ggaebiz/presentation/ui/home/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package com.ggaebiz.ggaebiz.presentation.ui.home
 
+import android.media.AudioManager
 import com.ggaebiz.ggaebiz.domain.usecase.SelectCharacterIdxUseCase
 import com.ggaebiz.ggaebiz.presentation.common.base.BaseViewModel
 
@@ -9,6 +10,7 @@ data class HomeState(
 )
 
 sealed interface HomeSideEffect {
+    data object NoticeVolumeOff : HomeSideEffect
     data object NavigateToSetting : HomeSideEffect
     data object CheckPermission : HomeSideEffect
 }
@@ -17,11 +19,15 @@ sealed interface HomeIntent {
     data class SelectCharacter(val selectCharacterIdx: Int) : HomeIntent
     data object ClickSettingButton : HomeIntent
     data class UpdatePermission(val isGranted: Boolean) : HomeIntent
+    data object PlayMentAudio : HomeIntent
 }
 
 class HomeViewModel(
     private val selectCharacterIdxUseCase: SelectCharacterIdxUseCase,
+    private val audioManager: AudioManager,
 ) : BaseViewModel<HomeState, HomeIntent, HomeSideEffect>(HomeState()) {
+
+    private val deviceVolume get() = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
 
     init {
         postSideEffect(HomeSideEffect.CheckPermission)
@@ -32,6 +38,7 @@ class HomeViewModel(
             is HomeIntent.ClickSettingButton -> clickSettingButton()
             is HomeIntent.SelectCharacter -> selectCharacter(intent.selectCharacterIdx)
             is HomeIntent.UpdatePermission -> updateState { it.copy(isGranted = intent.isGranted) }
+            is HomeIntent.PlayMentAudio -> checkVolume()
         }
     }
 
@@ -44,4 +51,9 @@ class HomeViewModel(
         postSideEffect(HomeSideEffect.NavigateToSetting)
     }
 
+    private fun checkVolume() {
+        if (deviceVolume == 0) {
+            postSideEffect(HomeSideEffect.NoticeVolumeOff)
+        }
+    }
 }
