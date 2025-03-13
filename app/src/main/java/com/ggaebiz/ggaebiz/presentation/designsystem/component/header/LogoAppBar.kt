6c44841c067391300076c7e0894c6c9b8e54f047
asheon12 <55980680+ashwon12@users.noreply.github.com>
2025-03-13 14:07:43 +0900

    [refactor] 설정 아이콘 벡터로 변경

--- app/src/main/java/com/ggaebiz/ggaebiz/presentation/designsystem/component/header/LogoAppBar.kt
+++ app/src/main/java/com/ggaebiz/ggaebiz/presentation/designsystem/component/header/LogoAppBar.kt
@@ -1,34 +1,35 @@
 package com.ggaebiz.ggaebiz.presentation.designsystem.component.header
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ggaebiz.ggaebiz.R
-import com.ggaebiz.ggaebiz.presentation.designsystem.component.button.GaeBizIconButton
-import com.ggaebiz.ggaebiz.presentation.designsystem.component.icon.GaeBizIcon
+import com.ggaebiz.ggaebiz.presentation.designsystem.theme.GaeBizTheme
 
 @Composable
 fun GaeBizLogoAppBar(
     modifier: Modifier = Modifier,
     logoDrawable: Int = R.drawable.ggaebiz_kor,
-    clickRightIcon : () -> Unit
+    clickRightIcon: () -> Unit,
 ) {
     Box(
-        modifier = modifier.fillMaxWidth().padding(horizontal = 20.dp)
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp)
     ) {
         Image(
             modifier = modifier
@@ -39,13 +40,20 @@ fun GaeBizLogoAppBar(
         )
 
         Image(
-            painter = painterResource(id = R.drawable.ic_setting),
-            contentDescription = "Clickable Image",
-            modifier = Modifier.size(48.dp).clickable {
-                clickRightIcon()
-            }.align(Alignment.CenterEnd)
+            modifier = Modifier
+                .size(48.dp)
+                .clip(
+                    CircleShape
+                )
+                .clickable {
+                    clickRightIcon()
+                }
+                .background(GaeBizTheme.colors.gray50)
+                .padding(vertical = 12.dp, horizontal = 16.dp)
+                .align(Alignment.CenterEnd),
+            painter = painterResource(id = R.drawable.icon_setting),
+            contentDescription = "Clickable Image"
         )
-
     }
 }
 
