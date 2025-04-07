31e2edeb295afa9b2d0fb6be5818df742ce6cab2
asheon12 <55980680+ashwon12@users.noreply.github.com>
2025-04-07 23:51:42 +0900

    [build] 깨비즈 v1.0.3_QA 1차 버전

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ggaebiz.ggaebiz"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1000201
-        versionName = "1.0.2"
+        versionCode = 1000300
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
