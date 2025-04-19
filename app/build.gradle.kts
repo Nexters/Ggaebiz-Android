23244381a078b9553604105d07293a9e804abdf4
seoggyui <likppi10@gmail.com>
2025-04-19 16:42:14 +0900

    [build] 깨비즈 1.0.3 - 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.ggaebiz.ggaebiz"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1000300
+        versionCode = 1000301
         versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
