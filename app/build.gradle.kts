124a69823fc4b39e0fb75f06964ef77e8f88e62b
seoggyui <likppi10@gmail.com>
2025-10-05 23:35:36 +0900

    [build] 깨비즈 1.0.4 버전 코드 싱크

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ggaebiz.ggaebiz"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1000302
-        versionName = "1.0.3"
+        versionCode = 1000401
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
