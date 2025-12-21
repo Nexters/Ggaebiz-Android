59df6ac55751ea99caf3ea54ce649ad5046cd5cb
seoggyui <likppi10@gmail.com>
2025-12-22 00:49:01 +0900

    [build] 깨비즈 1.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ggaebiz.ggaebiz"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1000401
-        versionName = "1.0.4"
+        versionCode = 1000501
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
