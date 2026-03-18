833c22d27469268acd02f73d24e6d2b78dd2d111
asheon12 <55980680+ashwon12@users.noreply.github.com>
2026-03-18 23:21:00 +0900

    1.0.6 버전업

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.ggaebiz.ggaebiz"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1000503
+        versionCode = 1000600
         versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
